3304be0a5ab61d6f03766f6410b6b81e39d49152
HMDuran <hanahmae.duran@gmail.com>
2023-08-10 22:56:50 +0800

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,11 @@ module.exports = {
   content: ["./public/index.html", "./public/pages/aklatan/aklatan.html"],
   theme: {
     colors: {
-      'black': '"#000',
       'neutral': {
         950: '#0A0A0A',
       },
       'zinc': {
-        100:'#F2F0F0',
+        100: '#F2F0F0',
       },
       'stone': {
         300: '#E7D7C1',
@@ -19,16 +18,25 @@ module.exports = {
         900: '#8B1C13',
       },
     },
-    spacing: {
-      '2': '5px',
-      '11': '46px',
-      '54': '220px',
-    },
     borderRadius: {
-      '20':'20px',
+      '20': '20px',
       '30': '30px',
     },
-    extend: {},
+    extend: {
+      spacing: {
+        '1.4' : '5px',
+        '11.5': '46px',
+        '12.5': '50px',
+        '37'  : '150px',
+        '42'  : '170px',
+        '54'  : '220px',
+        '78'  : '300px',
+        '100' : '400px',
+      },
+      gridTemplateRows: {
+       '7': 'repeat(7, minmax(0, 1fr))' 
+      }
+    },
   },
   plugins: [],
 }
