382c30226d4579726dcf9df04a3d463638124f90
HMDuran <hanahmae.duran@gmail.com>
2023-09-14 22:18:07 +0800

    Add id

--- public/js/script.js
+++ public/js/script.js
@@ -8,39 +8,39 @@ function Book (title, author, pages, read) {
         return (`${this.title} ${this.author}, ${this.pages}, ${this.read}`)
     };
 }
-// Add and Sort Button
+
+// DOM elements
 const addBookBtn     = document.getElementById('add-book-btn');
 const sortBooks      = document.getElementById('sort-btn');
-// Form
 const modal          = document.getElementById('modal');
 const closeBtn       = document.getElementById('close-modal');
 const submitBtn      = document.getElementById('submit-btn');
-// Inputs
 const titleInput     = document.getElementById('title');
 const authorInput    = document.getElementById('author');
 const pagesInput     = document.getElementById('pages');
 const statusSelected = document.getElementById('status');
-// Table
 const bookTable      = document.getElementById('book-table');
- // Keep track of the row being edited
-let editedRowIndex = -1;
-// Track the current sorting state
-let isSorted = JSON.parse(localStorage.getItem('isSorted')) || false;
 
-// Initialize book counts
+
+let editedRowIndex = -1;
+let isSorted = JSON.parse(localStorage.getItem('isSorted')) || false;
 let totalBooksCount = 0;
 let booksReadCount = 0;
 let booksNotReadCount = 0;
+const itemsPerPage = 10;
+let currentPage = 1;
 
 // Open the modal form
 function openModal() {
     modal.classList.remove('hidden');
 }
+
 // Close the modal form
 function closeModal() {
     modal.classList.add('hidden');
     clearForm();
 }
+
 // Handle form submission within the modal
 modal.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
@@ -49,51 +49,54 @@ modal.querySelector('form').addEventListener('submit', function(event) {
     const author = authorInput.value;
     const pages  = pagesInput.value;
     const status = statusSelected.value;
+    
     if (editedRowIndex !== -1) {
-        // If editing an existing book
         const newRow = bookTable.rows[editedRowIndex];
         const cells  = newRow.cells;
-        // Update cell values with edited data
+        
         cells[0].textContent = title;
         cells[1].textContent = author;
         cells[2].textContent = pages;
         cells[3].textContent = status;
-        // Update the data in local storage after editing
+        
         updateLocalStorage();
         closeModal();
     } else {
-         // If adding a new book
         const newBook = new Book(title, author, pages, status);
-        // Insert a new row in the table
+       
         const newRow      = bookTable.insertRow();
         const titleCell   = newRow.insertCell(0);
         const authorCell  = newRow.insertCell(1);
         const pagesCell   = newRow.insertCell(2);
         const statusCell  = newRow.insertCell(3);
         const actionsCell = newRow.insertCell(4);
-        // Populate cells with book data
+       
         titleCell.textContent  = newBook.title;
         authorCell.textContent = newBook.author;
         pagesCell.textContent  = newBook.pages;
         statusCell.textContent = newBook.read;
         actionsCell.innerHTML  = '<button class="edit-btn">Edit</button> <button class="remove-btn">Delete</button>';
-        // Save the data to local storage after adding a new book
+        
         saveToLocalStorage(newBook);
         closeModal();
     }
 });
+
 // Event listener for "Add Book" button
 addBookBtn.addEventListener('click', function() {
     openModal();
     clearForm();
     editedRowIndex = -1; 
 });
+
 // Event listener for "Add Book" button
 closeBtn.addEventListener('click', closeModal)
+
 // Event listener to handle book editing/removal
 bookTable.addEventListener('click', function(event) {
     const target = event.target;
     const row    = target.closest('tr');
+
     if (target.classList.contains('edit-btn')) {
         openModal();
         editedRowIndex = row.rowIndex;
@@ -103,14 +106,17 @@ bookTable.addEventListener('click', function(event) {
         deleteFromLocalStorage(row);
     }
 });
+
 // Populate the form with data from a selected row
 function populateFormWithRowData(row) {
     const cells          = row.cells;
+
     titleInput.value     = cells[0].textContent;
     authorInput.value    = cells[1].textContent;
     pagesInput.value     = cells[2].textContent;
     statusSelected.value = cells[3].textContent;
 }
+
 // Clear the form inputs and reset editedRowIndex
 function clearForm() {
     titleInput.value     = "";
@@ -119,6 +125,7 @@ function clearForm() {
     statusSelected.value = "";
     editedRowIndex       = -1;
 }
+
 // Function to save a new book to local storage
 function saveToLocalStorage(book) {
     const existingBooks = JSON.parse(localStorage.getItem('books')) || [];
@@ -132,6 +139,7 @@ function saveToLocalStorage(book) {
 function updateLocalStorage() {
     const rows = Array.from(bookTable.rows).slice(1);
     const updatedBooks = rows.map(row => {
+
         return {
             title:  row.cells[0].textContent,
             author: row.cells[1].textContent,
@@ -160,7 +168,6 @@ function updateCounts() {
     booksReadCount = Array.from(bookTable.rows).slice(1).filter(row => row.cells[3].textContent === 'Read').length;
     booksNotReadCount = totalBooksCount - booksReadCount;
 
-    // Update the HTML elements displaying the counts
     document.querySelector('.cards .number').textContent       = totalBooksCount;
     document.querySelectorAll('.cards .number')[1].textContent = booksReadCount;
     document.querySelectorAll('.cards .number')[2].textContent = booksNotReadCount;
@@ -170,11 +177,10 @@ function updateCounts() {
 // Load data from local storage when the page loads
 function loadBooksFromLocalStorage() {
     const existingBooks = JSON.parse(localStorage.getItem('books')) || [];
-    // Clear existing rows from the table
+   
     while (bookTable.rows.length > 1) {
         bookTable.deleteRow(1);
     }
-    // Sort the existingBooks array based on title
     existingBooks.sort((bookA, bookB) => {
         const titleA = bookA.title.toLowerCase();
         const titleB = bookB.title.toLowerCase();
@@ -187,6 +193,7 @@ function loadBooksFromLocalStorage() {
         const pagesCell   = newRow.insertCell(2);
         const statusCell  = newRow.insertCell(3);
         const actionsCell = newRow.insertCell(4);
+
         titleCell.textContent  = book.title;
         authorCell.textContent = book.author;
         pagesCell.textContent  = book.pages;
@@ -194,24 +201,28 @@ function loadBooksFromLocalStorage() {
         actionsCell.innerHTML  = '<button class="edit-btn">Edit</button> <button class="remove-btn">Delete</button>';
     });
 }
+
 // Load data from local storage when the page loads
 loadBooksFromLocalStorage();
 // Populate the table based on the initial sorting state
 populateTable();
+
 // Event listener for the "Sort" button
 sortBooks.addEventListener('click', function() {
     toggleSort();
     populateTable();
 });
+
 // Function to toggle sorting state
 function toggleSort() {
     isSorted = !isSorted; 
     localStorage.setItem('isSorted', JSON.stringify(isSorted));
 }
+
 // Function to load and sort data from local storage
 function loadAndSortData() {
     const existingBooks = JSON.parse(localStorage.getItem('books')) || [];
-    // Sort the existingBooks array based on title
+   
     existingBooks.sort((bookA, bookB) => {
         const titleA = bookA.title.toLowerCase();
         const titleB = bookB.title.toLowerCase();
@@ -219,37 +230,63 @@ function loadAndSortData() {
     });
 }
 
-// Initialize the counts and display them
-updateCounts();
-
-// Function to populate table with data
-function populateTable() {
-    // Clear existing rows from the table
+function loadBooksFromLocalStorage() {
+    const existingBooks = JSON.parse(localStorage.getItem('books')) || [];
+   
     while (bookTable.rows.length > 1) {
         bookTable.deleteRow(1);
     }
-    const data = JSON.parse(localStorage.getItem('books')) || [];
-    if (isSorted) {
-        data.sort((bookA, bookB) => {
-            const titleA = bookA.title.toLowerCase();
-            const titleB = bookB.title.toLowerCase();
-            return titleA.localeCompare(titleB);
-        });
-    }
-    data.forEach(book => {
+    existingBooks.sort((bookA, bookB) => {
+        const titleA = bookA.title.toLowerCase();
+        const titleB = bookB.title.toLowerCase();
+        return titleA.localeCompare(titleB);
+    });
+    existingBooks.forEach(book => {
         const newRow      = bookTable.insertRow();
         const titleCell   = newRow.insertCell(0);
         const authorCell  = newRow.insertCell(1);
         const pagesCell   = newRow.insertCell(2);
         const statusCell  = newRow.insertCell(3);
         const actionsCell = newRow.insertCell(4);
-        titleCell.textContent   = book.title;
-        authorCell.textContent  = book.author;
-        pagesCell.textContent   = book.pages;
-        statusCell.textContent  = book.read;
-        actionsCell.innerHTML   = '<button class="edit-btn">Edit</button> <button class="remove-btn">Delete</button>';
+
+        titleCell.textContent  = book.title;
+        authorCell.textContent = book.author;
+        pagesCell.textContent  = book.pages;
+        statusCell.textContent = book.read;
+        actionsCell.innerHTML  = '<button class="edit-btn">Edit</button> <button class="remove-btn">Delete</button>';
     });
+
+    // Check whether to display pagination controls
+    const totalNumPages = Math.ceil(existingBooks.length / itemsPerPage);
+    const pagination = document.getElementById('pagination');
+    if (totalNumPages > 1) {
+        pagination.style.display = 'flex'; // Show pagination controls
+    } else {
+        pagination.style.display = 'none'; // Hide pagination controls
+    }
 }
+
+// Initialize the counts and display them
+updateCounts();
+
+populateTable(currentPage);
+
+// Event listener for the "Next" button
+document.getElementById('next-btn').addEventListener('click', function () {
+    if (currentPage < Math.ceil(totalBooksCount / itemsPerPage)) {
+        currentPage++;
+        populateTable(currentPage);
+    }
+});
+
+// Event listener for the "Previous" button
+document.getElementById('prev-btn').addEventListener('click', function () {
+    if (currentPage > 1) {
+        currentPage--;
+        populateTable(currentPage);
+    }
+});
+
 // Sort the book list alphabetically by title
 function sortBooksAlphabetically() {
     const rows       = Array.from(bookTable.rows).slice(1); 
@@ -258,11 +295,9 @@ function sortBooksAlphabetically() {
         const titleB = rowB.cells[0].textContent.toLowerCase();
         return titleA.localeCompare(titleB);
     });
-     // Remove existing rows from the table
     while (bookTable.rows.length > 1) {
         bookTable.deleteRow(1);
     }
-    // Append sorted rows back to the table
     sortedRows.forEach(row => {
         bookTable.appendChild(row);
     });
